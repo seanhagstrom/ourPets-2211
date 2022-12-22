6f6fad69bade66833adfe4e013a394a182a8a68d
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-12-22 17:07:29 -0500

    add ssl to client instance

--- db/client.js
+++ db/client.js
@@ -1,7 +1,13 @@
 const { Client } = require('pg');
 const { DATABASE_URL } = process.env;
 const connectionString = DATABASE_URL || 'postgres://localhost:5432/petpals';
-const client = new Client(connectionString);
+const client = new Client({
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 // client.connect().then(() => console.log('connected to petpals'));
 
